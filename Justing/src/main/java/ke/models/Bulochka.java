61e860b2b62f12bf133a80b16048ee6ac52d5cff
Milovanie <zirka7777@gmail.com>
2023-08-29 22:41:09 +0200

    main Branch
    add new classes :
    Bicycle, Car, SealedClasses, Vehicle

--- Justing/src/main/java/ke/models/Bulochka.java
+++ Justing/src/main/java/ke/models/Bulochka.java
@@ -2,12 +2,9 @@ package ke.models;
 
 public class Bulochka {
 
-	public static void main(String[] args) {
-
-	}
-
 	public String sayProvet() {
 		return "privet Bulo4ka";
-
 	}
+	
+	
 }
---
+++ Justing/src/main/java/ke/models/SealedClasses.java
@@ -0,0 +1,4 @@
+package ke.models;
+
+public class SealedClasses {
+}
